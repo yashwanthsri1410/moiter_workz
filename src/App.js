de894bb6ce7754d38e7517f4185d8741426bec4b
yashwanth sriniavs <yashwanthsri1410@gmail.com>
2025-06-05 10:56:29 +0530

    deleted error files

--- src/App.js
+++ src/App.js
@@ -9,12 +9,6 @@ import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Usercreation from "./pages/Usercreation";
 
-import Manager from "./pages/dashboard/Manager";
-import Senior from "./pages/dashboard/Senior";
-import Junior from "./pages/dashboard/Junior";
-import Financemanager from "./pages/dashboard/Financemanager";
-import Budgetanalyst from "./pages/dashboard/Budgetanalyst";
-import Costaccountant from "./pages/dashboard/Costaccountant";
 
 function App() {
   const [role, setRole] = useState("");
@@ -27,30 +21,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login setRole={setRole} />} />
         <Route path="/Usercreation" element={<Usercreation />} />
-        <Route
-          path="/dashboard"
-          element={
-            isAuthenticated ? (
-              role === "Manager" ? (
-                <Manager />
-              ) : role === "Senior Employee" ? (
-                <Senior />
-              ) : role === "Junior Employee" ? (
-                <Junior />
-              ) : role === "Finance Manager" ? (
-                <Financemanager />
-              ) : role === "Budget Analyst" ? (
-                <Budgetanalyst />
-              ) : role === "Cost Accountant" ? (
-                <Costaccountant />
-              ) : (
-                <Navigate to="/login" />
-              )
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
+       
       </Routes>
     </Router>
   );
